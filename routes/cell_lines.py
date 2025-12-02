fbee56b751ff65e5fb7e2f8c45c046a371939e9c
mattbocc <matthew.boccalon@live.ca>
2025-12-02 12:35:58 -0500

    feat: added synonyms detection from new table. added loadin of diseases into cell lines query based on ORM layer relationship.

--- routes/cell_lines.py
+++ routes/cell_lines.py
@@ -4,11 +4,11 @@ from typing import List
 from urllib.parse import quote_plus
 from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy import create_engine, select, or_
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, selectinload
 import pandas as pd
 from dotenv import load_dotenv
-from models.tables import CellLines
-# from models.output import OutputFormat
+from models.tables import CellLines, CellLineSynonyms, CellLineDisease
+from models.output import OutputFormat
 
 load_dotenv(override=True)
 
@@ -41,9 +41,9 @@ async def get_cell_lines(
         description="Cell line names or cellosaurus accession id's (comma seperated)",
         example="HL-60,HeLa,CVCL_0060,CVCL_2030",
     ),
-    # format: OutputFormat = Query(
-    #     OutputFormat.json, description="Output format: `json` or `csv`."
-    # ),
+    format: OutputFormat = Query(
+        OutputFormat.json, description="Output format: `json` or `csv`."
+    ),
     session=Depends(get_db_session),
 ):
     if not cell_lines:
@@ -51,14 +51,24 @@ async def get_cell_lines(
             status_code=400, detail="Need to include at least one drug to get output"
         )
 
-    cell_line_list = [drug for drug in cell_lines.split(",")]
+    cell_line_list = [cell_line for cell_line in cell_lines.split(",")]
 
     conditions = []
-    for name in cell_line_list:
-        conditions.append(CellLines.cell_line_name.ilike(name))
-        conditions.append(CellLines.accession.ilike(name))
-        conditions.append(CellLines.synonyms.ilike(name))
+    for identifier in cell_line_list:
+        conditions.append(CellLines.cell_line_name.ilike(identifier))
+        conditions.append(CellLines.accession.ilike(identifier))
+        conditions.append(CellLineSynonyms.synonym.ilike(identifier))
 
-    rows = session.query(CellLines).filter(or_(*conditions)).distinct().all()
+    rows = (
+        session.query(CellLines)
+        .options(selectinload(CellLines.diseases))
+        .outerjoin(
+            CellLineSynonyms,
+            CellLines.accession == CellLineSynonyms.cellosaurus_accession,
+        )
+        .filter(or_(*conditions))
+        .distinct()
+        .all()
+    )
 
     return rows
