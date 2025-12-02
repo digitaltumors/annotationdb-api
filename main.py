a6cc488a252592b652770f896575cf792761af17
mattbocc <matthew.boccalon@live.ca>
2025-12-02 14:52:43 -0500

    feat: added redirect from base url to docs url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from routes.drugs import router as drugs_router
 from routes.cell_lines import router as cell_lines_router
 
@@ -7,6 +8,6 @@ app.include_router(drugs_router)
 app.include_router(cell_lines_router)
 
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse(url="/docs")
